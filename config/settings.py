404f8a31c197ba78a46a71d7e1d1aa199f90fbae
majid-757 <majid.asadollahi95@gmail.com>
2022-05-18 21:56:35 +0430

    config cashe in django

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,9 @@ INTERNAL_IPS = [
 ]
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
